d20a6be0d9be90eec0c115679f2c1f15b83d8116
Rohan Sharma <namitsatyam@gmail.com>
2020-03-03 18:28:35 +0530

    adding edit and delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
+	  http_basic_authenticate_with name: "rohan", password: "shambhunath@123", except: [:index, :show]
 
 def index
-	@articles= Article.all
+	@articles = Article.all
 end
 
 def show
@@ -33,6 +34,13 @@ def update
 	else 
 		render 'edit'
 	end
+end
+
+def destroy
+	@article=Article.find(params[:id])
+	@article.destroy
+
+	redirect_to articles_path
 end
 
 private
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
  root 'hello#rohan'
   get 'hello/rohan'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :articles
+  resources :articles do
+  	resources :comments
+  end
  
 end
