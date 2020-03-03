be78abffb2d2301abf43ace6d0c226387bd3227b
Rohan4045 <52060192+Rohan4045@users.noreply.github.com>
2020-03-03 10:46:41 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+ root 'article#index'
   get 'hello/rohan'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles
-  root 'hello#rohan'
+ 
 end
